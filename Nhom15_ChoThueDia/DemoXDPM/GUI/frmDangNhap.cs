fe39a558e4ed7b56626a6bb861eabe00b03cbce8
ngan binh <nguyenlenganbinh0505@gmail.com>
2020-12-04 13:19:54 +0700

    Chinh sua CSDL va combobox ben frmQLDatDia

---
+++ Nhom15_ChoThueDia/DemoXDPM/GUI/frmDangNhap.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap(int x)
+        {
+            InitializeComponent();
+            int check = x;
+            if (check == 0)
+            {
+                frmFlashScreen fFlashScreen = new frmFlashScreen();
+                fFlashScreen.ShowDialog();
+            }
+            else if (check == 1)
+            {
+
+            }
+        }
+
+        private void picShutdown_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            txtTaiKhoan.Text = txtTaiKhoan.Text.ToUpper();
+            if(txtTaiKhoan.Text =="" || txtMatKhau.Text == "")
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không được để trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (txtTaiKhoan.Text == "ADMIN" && txtMatKhau.Text == "123")
+                {
+                    frmMenuQuanLy rmQUly = new frmMenuQuanLy();
+                    rmQUly.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Tài khoản hoặc mật khẩu không đúng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnTiepTuc_Click(object sender, EventArgs e)
+        {
+            frmMenuNhanVien rmNhanVien = new frmMenuNhanVien();
+            rmNhanVien.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Nhom15_ChoThueDia/DemoXDPM/GUI/frmQLKhachHang.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace GUI
+{
+    partial class frmQLKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmQLKhachHang
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1783, 1055);
+            this.Name = "frmQLKhachHang";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý khách hàng";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
